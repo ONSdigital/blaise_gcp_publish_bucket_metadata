31c94a2e02b5c8ab37dda38b5dba2705c0eaa1d9
Kimbed <Kimbed@ONS24158>
2019-12-05 18:10:45 +0000

    LU-4648 Test code so far for encrypt file

--- blaise_bucket/main.py
+++ blaise_bucket/main.py
@@ -1,3 +1,39 @@
+from cryptography.fernet import Fernet
+
+
+def main():
+
+    # load the key
+    public_key_fromfile = load_key()
+    encryptFile("blaise_bucket/Test Files/input_file.txt", public_key_fromfile)
+
+
+def load_key():
+    """
+    Loads the key from the test public gpg file
+    """
+    return open("blaise_bucket/keys/ons_blaise5_gpg_key_a_test.gpg", "rb").read()
+
+
+def encryptFile(input_file, public_key_fromfile):
+    """
+    Given a filename (str) and key (bytes), it encrypts the file and write it back out to a new file
+    """
+    output_file = "blaise_bucket/Test Files/output_file.txt"
+
+    with open(input_file, "rb") as f:
+        # read all input file data
+        input_file_data = f.read()
+
+    fernet = Fernet(public_key_fromfile)
+
+    # encrypt data
+    encrypted_data = fernet .encrypt(input_file_data)
+
+    with open(output_file, 'wb') as f:
+        f.write(encrypted_data)
+
+
 def pubFileMetaData(data, context):
     import json
     import os
